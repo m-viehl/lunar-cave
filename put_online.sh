94df42fcd6eb0da563715e91093a20252b67fe74
Max Viehl <15348204+m-viehl@users.noreply.github.com>
2022-07-02 17:24:47 +0200

    Change html name in publishing script

--- put_online.sh
+++ put_online.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp build/main.html coruscant:to_put_online/lunar.html
+scp build/index.html coruscant:to_put_online/index.html
 scp build/main.js coruscant:to_put_online/main.js
 ssh coruscant ./put_online.sh
 
