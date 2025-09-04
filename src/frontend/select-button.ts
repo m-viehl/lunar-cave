ab152f3ebe7236d6020098845b89c3b603172803
Max Viehl <15348204+m-viehl@users.noreply.github.com>
2025-09-04 11:33:21 +0200

    Finish selectbutton component
    Now simpler string-based option/value input and additional init event

--- src/frontend/select-button.ts
+++ src/frontend/select-button.ts
@@ -45,91 +45,77 @@ export class SelectButton extends LitElement {
     // state
     static properties = {
         label: { type: String },
-        value: { type: String }
+        value: { type: String },
+        options: { type: String },
+        values: { type: String },
+        default_index: { type: Number },
     };
 
-    value: string = "" // TODO how to initialize such properties in lit?
+    value: string = ""
     label: string = ""
+    options: string = ""
+    values: string = ""
+    default_index: number = 0
 
-    connectedCallback(): void {
-      console.log("SELECT connected")
-    }
+    options_list: { option: string, value: string, id: string }[] = []
 
+    willUpdate(): void {
+        let option_list = this.options.split(";")
+        let value_list = this.values.split(";")
+        this.value = value_list[this.default_index]
 
-    firstUpdated(): void {
-        /*
-        It is important to use firstUpdated, otherwise the slotted options
-        might not be present yet and render won't trigger!
-        */
-        const options = this._getOptions();
-        const selected = options.find(o => o.hasAttribute('selected'));
-        if (selected) {
-            this.value = selected.value;
-        } else if (options.length) {
-            this.value = options[0].value;
-        } else {
-            this.value = ""
-            console.log("setting value to ''")
+        // read current option from localstorage
+        let val = window.localStorage.getItem(this.get_storage_key())
+        if (
+            val !== null && // any value stored
+            value_list.indexOf(val) >= 0 // and valid option
+        ) {
+            this.value = val;
         }
-        this.restore(); // does nothing if no value is present
+        this.persist(); // this overwrites invalid options that may be in localstorage
 
-        console.log("BUTTON firstupdated, value is ", this.value)
-        // TODO parse options once, write to object property.
-        // then, in restore, check that option is still valid (when changing options, we must
-        // not load old ones!)
+        this.options_list = option_list.map((option, index) => ({
+            option: option,
+            value: value_list[index],
+            id: `option_${option}`
+        }));
+
+        // after initialization, fire a changed event
+        this.dispatchEvent(new Event('init', { bubbles: true, composed: true }));
     }
 
-    private _getOptions(): HTMLOptionElement[] {
-        return Array.from(this.querySelectorAll('option'));
-    }
 
-    private _onChange(e: Event): void {
+
+    private on_change(e: Event): void {
         const target = e.target as HTMLInputElement;
         this.value = target.value;
         this.dispatchEvent(new Event('change', { bubbles: true, composed: true }));
         this.persist();
     }
 
+    private get_storage_key() {
+        return LOCALSTORAGE_PREFIX + this.label.toUpperCase().replace(/[^a-zA-Z0-9]+/g, '');
+    }
+
     /**
      * Writes this element's currently selected option to localstorage
      */
     private persist() {
-        const groupId = this.label.toUpperCase().replace(/\s+/g, '_');
-        let storage_key = LOCALSTORAGE_PREFIX + groupId
-        window.localStorage.setItem(storage_key, this.value);
-    }
-
-    /**
-     * Reads and selects the currently selected option from localstorage
-     */
-    private restore() {
-        const groupId = this.label.toUpperCase().replace(/\s+/g, '_');
-        let storage_key = LOCALSTORAGE_PREFIX + groupId
-        let val = window.localStorage.getItem(storage_key)
-        if (val !== null) {
-            this.value = val;
-        }
+        window.localStorage.setItem(this.get_storage_key(), this.value);
     }
 
 
     render() {
-        const options = this._getOptions();
-        const groupId = this.label.toLowerCase().replace(/\s+/g, '_') + '_group';
+        const groupId = this.get_storage_key();
         return html`
-      <p>${this.label}</p>
-      <div class="radio-group" id=${groupId}>
-        ${options.map(o => html`
-          <input
-            type="radio"
-            id=${o.id}
-            name=${groupId}
-            value=${o.value}
-            ?checked=${this.value === o.value}
-            @change=${this._onChange}
-          >
-          <label for=${o.id}>${o.textContent}</label>
+    <p>${this.label}</p>
+    <div class="radio-group" id=${groupId}>
+        ${this.options_list.map(o => html`
+        <input type="radio" id=${o.id} name=${groupId} value=${o.value} ?checked=${this.value===o.value}
+            @change=${this.on_change}>
+        <label for=${o.id}>${o.option}</label>
         `)}
-      </div>
+    </div>
     `;
     }
 
