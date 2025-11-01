545b8e730fde929a7070d0a9f4756b6b8a7eae4e
Max Viehl <15348204+m-viehl@users.noreply.github.com>
2025-11-01 12:13:22 +0100

    Fix div in span issue

--- src/frontend/vue_ui/Help.vue
+++ src/frontend/vue_ui/Help.vue
@@ -3,21 +3,21 @@
         <h2>Controls</h2>
         <p>
             Use
-        <div class="keygrid">
-            <span></span>
-            <span class="key">W</span>
-            <span></span>
-            <span class="key">A</span>
-            <span></span>
-            <span class="key">D</span>
-        </div> or <div class="keygrid">
-            <span></span>
-            <span class="key">↑</span>
-            <span></span>
-            <span class="key">←</span>
-            <span></span>
-            <span class="key">→</span>
-        </div> to control the ship.
+            <span class="keygrid">
+                <span></span>
+                <span class="key">W</span>
+                <span></span>
+                <span class="key">A</span>
+                <span></span>
+                <span class="key">D</span>
+            </span> or <span class="keygrid">
+                <span></span>
+                <span class="key">↑</span>
+                <span></span>
+                <span class="key">←</span>
+                <span></span>
+                <span class="key">→</span>
+            </span> to control the ship.
         </p>
 
         <p v-if="initmode">
@@ -31,7 +31,7 @@
 
 
 <script lang="ts" setup>
-const props = defineProps<{
-  initmode: boolean;
+defineProps<{
+    initmode:     olean;
 }>();
 </script>
